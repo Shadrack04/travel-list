45359f4e7eb4707b182b931e2206956ef092af19
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-12-05 02:58:13 +0100

    Create and style the Stat component

---
+++ src/components/Stats.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Stats() {
+  return (
+    <div className=" row-span-2 bg-[#76c7ad] text-center font-light py-8">
+      You have 0 item on your list, and you already packed 0(0%)
+    </div>
+  );
+}
+
+export default Stats;
+
+// .stats {
+//   background-color: #76c7ad;
+//   text-align: center;
+//   font-weight: 700;
+//   padding: 3.2rem 0;
+// }
