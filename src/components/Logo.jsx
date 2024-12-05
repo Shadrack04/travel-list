12422a2a2c08fa13a90bcae6db206f2c4641b03f
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-12-05 02:55:18 +0100

    Create and style the logo section

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className=" row-span-2 text-center bg-[#f4a226] pt-8 content-center">
+      <h1 className="logo font-[Monoton] text-6xl font-bold sm:word-spacing-[30px]">
+        🌴 FAR AWAY 💼
+      </h1>
+    </div>
+  );
+}
